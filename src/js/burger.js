55ad8133b17ecc5ad8f2ffc5ac5ede1b4f50f520
serezha3179 <sergioss31@rambler.ru>
2022-08-04 11:11:16 +0300

    costs

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,8 +1,11 @@
+let header = document.querySelector('.header');
 let headerBurger = document.querySelector('.header__burger');
 let headerNav = document.querySelector('.header__nav');
 let headerLink = document.querySelectorAll('.header__nav-link');
+let body = document.querySelector('body');
 headerBurger.addEventListener('click' , () => {
     headerBurger.classList.toggle('active');
-    headerNav.classList.toggle('active')
-
+    headerNav.classList.toggle('active');
+    header.classList.toggle('active');
+    body.classList.toggle('lock');
 })
