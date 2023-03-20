69f7c5370d13f7eea3bb5f21f6f243812676007c
serezha3179 <sergeiabramkin.79@gmail.com>
2023-03-20 22:50:45 +0300

    fixed links

--- src/js/map.js
+++ src/js/map.js
@@ -1,5 +1,3 @@
 
 let clientScreenHeight = document.documentElement.clientHeight;
-// console.log(clientScreenHeight)
 let heightContactsMap = document.querySelector('.contacts__map').style.height = clientScreenHeight - 10 + 'px';
-// console.log(heightContactsMap);
